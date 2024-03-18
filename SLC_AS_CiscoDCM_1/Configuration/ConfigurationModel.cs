e9ee8c220f6c1e54c72e7f31011c359df4e23984
Robbie Demaegdt <robbie.demaegdt@skyline.be>
2024-03-18 10:17:09 +0100

    Add project files.

---
+++ SLC_AS_CiscoDCM_1/Configuration/ConfigurationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLC_AS_CiscoDCM_1.Configuration
+{
+    public class ConfigurationModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ElementName { get; set; }
+
+        public string ElementIp { get; set; }
+    }
+}
---
+++ SLC_AS_CiscoDCM_1/Configuration/ConfigurationView.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLC_AS_CiscoDCM_1.Configuration
+{
+    public class ConfigurationView : Dialog
+    {
+        public readonly TextBox Username;
+        public readonly PasswordBox Password;
+        private readonly Label username;
+        private readonly Label password;
+        private readonly Label elementName;
+        private readonly Label elementIp;
+
+        public ConfigurationView(Engine engine) : base(engine)
+        {
+            Title = "Configuration";
+            username = new Label()
+            {
+                Text = "Username: ",
+            };
+
+            password = new Label()
+            {
+                Text= "Password: ",
+            };
+
+            elementName = new Label()
+            {
+                Text = "Element Name: ",
+            };
+
+            elementIp = new Label()
+            {
+                Text = "Element IP: ",
+            };
+
+            Password = new PasswordBox(true);
+            SetupLayout();
+        }
+
+        public void SetupLayout()
+        {
+            AddWidget(username, 0, 0);
+            AddWidget(password, 1, 0);
+            AddWidget(Password, 1, 1);
+            AddWidget(elementName, 2, 0);
+            AddWidget(elementIp, 3, 0);
+        }
+    }
+}
